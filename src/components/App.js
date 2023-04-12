fccc1461a91c6b3c5dff35126d8d0055559e93a2
Gabrielle Glasgow <g.glasgow91@gmail.com>
2023-04-12 17:07:05 -0400

    completed lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import About from "./About"
 
 function NavBar() {
   return (
@@ -16,7 +17,9 @@ function Home() {
   );
 }
 
-{/* write an <About> component here */}
+{/* write an <About> component here */ }
+
+
 
 
 function App() {
@@ -24,7 +27,9 @@ function App() {
     <div>
       <NavBar />
       {/* add the <Home> component here */}
+      <Home />
       {/* add your <About> component here */}
+      <About />
     </div>
   );
 }
